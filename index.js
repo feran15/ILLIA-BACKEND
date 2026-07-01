3c4628582e591cb13da9be0d481f4f28d671e01b
unknown <abodundepaul2022@gmail.com>
2026-07-01 02:52:30 +0100

    CORS

--- index.js
+++ index.js
@@ -14,15 +14,25 @@ const mediaRoutes = require('./routes/media');
 const app  = express();
 const PORT = process.env.PORT || 5000;
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'https://illia-eight.vercel.app'
+];
+
 app.use(cors({
-  origin:         'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true,
-  methods:        ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Session-ID'],
   exposedHeaders: ['X-Session-ID'],
-  maxAge:         3600,
+  maxAge: 3600,
 }));
-
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
